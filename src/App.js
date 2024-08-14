4ffffbcdac7e6c972aec33f7bca18b21fe2fddc8
NathanaelOliver <no2@email.sc.edu>
2024-08-14 18:43:27 -0400

    template mostly finished

--- src/App.js
+++ src/App.js
@@ -18,6 +18,36 @@ const projects = [
     link: "https://example.com",
     description: "This is a description for Project 2.",
   },
+  {
+    title: "Project 1",
+    picture: "https://via.placeholder.com/150",
+    caption: "Caption 1",
+    link: "https://example.com",
+    description:
+      "This is a description for Project 1. It should be long enough to test the truncation. What if this text is obscenely long. Will it go around to the next line and cycle around. I wonder what the word is called for htat. Its called wrapping. I wonder if the text is obscenely long if it will wrap and i wonder if the tile will grow in height to mathc the size of the text. i willl use this as a test to find out.",
+  },
+  {
+    title: "Project 2",
+    picture: "https://via.placeholder.com/150",
+    caption: "Caption 2",
+    link: "https://example.com",
+    description: "This is a description for Project 2.",
+  },
+  {
+    title: "Project 1",
+    picture: "https://via.placeholder.com/150",
+    caption: "Caption 1",
+    link: "https://example.com",
+    description:
+      "This is a description for Project 1. It should be long enough to test the truncation. What if this text is obscenely long. Will it go around to the next line and cycle around. I wonder what the word is called for htat. Its called wrapping. I wonder if the text is obscenely long if it will wrap and i wonder if the tile will grow in height to mathc the size of the text. i willl use this as a test to find out.",
+  },
+  {
+    title: "Project 2",
+    picture: "https://via.placeholder.com/150",
+    caption: "Caption 2",
+    link: "https://example.com",
+    description: "This is a description for Project 2.",
+  },
   // Add more projects as needed
 ];
 
@@ -46,19 +76,35 @@ function App() {
             }`}
             onClick={() => handleTileClick(index)}
           >
-            <div className="ProjectPicture">
-              <img src={project.picture} alt={project.caption} />
-            </div>
-            <div className="ProjectContent">
-              <h2 className="ProjectTitle">{project.title}</h2>
-              <p className="ProjectDescription">{project.description}</p>
+            <div
+              className={`ProjectTileLeftColumn ${
+                expandedIndex === index ? "expanded" : ""
+              }`}
+            >
+              <div className="ProjectPicture">
+                <img src={project.picture} alt={project.caption} />
+              </div>
               {expandedIndex === index && (
-                <>
-                  <p className="ProjectCaption">{project.caption}</p>
-                  <a href={project.link} className="ProjectLink">
-                    Learn More
-                  </a>
-                </>
+                <p className="ProjectCaption">{project.caption}</p>
+              )}
+            </div>
+            <div
+              className={`ProjectTileRightColumn ${
+                expandedIndex === index ? "expanded" : ""
+              }`}
+            >
+              <h2 className="ProjectTitle">{project.title}</h2>
+              <p
+                className={`ProjectDescription ${
+                  expandedIndex === index ? "expanded" : ""
+                }`}
+              >
+                {project.description}
+              </p>
+              {expandedIndex === index && (
+                <a href={project.link} className="ProjectLink">
+                  Learn More
+                </a>
               )}
             </div>
           </div>
